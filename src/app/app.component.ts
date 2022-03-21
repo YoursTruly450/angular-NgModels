b5b5792faa421cc5c2fec93e5c4a2ed665cf44a5
meluferiev <meluferiev@axbit.ru>
2022-03-21 11:01:24 +0400

    feat: finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-services';
+  appState = 'on';
+
+  handleChange() {
+    console.log(this.appState);
+  }
 }
